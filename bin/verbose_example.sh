9143a3aa553ffcf9a7dadf230b6c4e58b5d9c8cd
Saccharine-Coal <69489271+Saccharine-Coal@users.noreply.github.com>
2022-03-31 16:52:09 -0700

    commit from separate repo

--- bin/verbose_example.sh
+++ bin/verbose_example.sh
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 cd ../docs/example
 
 python3 ../src/limits_from_xyz.py Te.mol
 
 python3 ../src/limits_from_xyz.py Te.xyz
+=======
+
+
+python3 ../src/limits_from_xyz.py ../docs/example/Te.mol
+
+python3 ../src/limits_from_xyz.py ../docs/example/Te.xyz
+>>>>>>> d06d929 (change branch name and pushed wrong local repo)
--- src/limits_from_xyz.py
+++ src/limits_from_xyz.py
@@ -2,7 +2,11 @@ import math
 import sys
 import os
 
+<<<<<<< HEAD
 from limits_from_xyz import get_geometry_data
+=======
+from xyz_from_file import get_geometry_data
+>>>>>>> d06d929 (change branch name and pushed wrong local repo)
 
 # ordered by atomic number
 VDW_RADII = {
@@ -34,12 +38,28 @@ def maximize(xyz, vdw_radius):
 def get_vdw_radius(atom: str, scale: int) -> float:
     return VDW_RADII[atom] * scale
 
+<<<<<<< HEAD
 def write_limits(x_lim, y_lim, z_lim, n_points=50):
+=======
+def input_to_bool(question: str) -> bool:
+    """Gets user y/n response and returns a bool corresponding to y or n."""
+    invalid = True
+    while invalid:
+        print(f"WARNING: {question} Continue? (y/n)")
+        response = input()
+        if response == "y" or response == "n":
+            invalid = False
+    return True if response == "y" else False
+
+
+def write_limits(x_lim, y_lim, z_lim, n_points=50, target_dir="."):
+>>>>>>> d06d929 (change branch name and pushed wrong local repo)
 
     def format_to_str(lim: tuple, n_points: int) -> str:
         return f"{n_points} {lim[0]} {lim[1]}\n"
 
     FILENAME = "limits.plot"
+<<<<<<< HEAD
     if os.path.exists(FILENAME):
         raise OSError
     else:
@@ -50,6 +70,22 @@ def write_limits(x_lim, y_lim, z_lim, n_points=50):
         print(f"Limits have been written to {FILENAME}")
 
 def main() -> None:
+=======
+    path_to_file = os.path.join(target_dir, FILENAME)
+    if input_to_bool(f"{path_to_file} already exists. OVERWRITE?"):
+
+        with open(path_to_file, "w+") as file:
+            file.write(format_to_str(x_lim, n_points))
+            file.write(format_to_str(y_lim, n_points))
+            file.write(format_to_str(z_lim, n_points))
+        print(f"Limits have been written to {path_to_file}")
+    else:
+        print(f"Limits have not been written.")
+
+
+def main() -> None:
+    """Main"""
+>>>>>>> d06d929 (change branch name and pushed wrong local repo)
     args = sys.argv.copy()
     args.pop(0)     # this is the path to this file
     path_to_file = args.pop(0)  # this is the first argument that is given that is not this file
@@ -86,5 +122,10 @@ def main() -> None:
         axis_limit = (math.floor(min(min_vals)), math.ceil(max(max_vals)))
         xyz_limits.append(axis_limit)
         print(f"limits for axis {i} = {axis_limit}")
+<<<<<<< HEAD
     write_limits(*xyz_limits)
+=======
+    head, tail = os.path.split(path_to_file)[:]
+    write_limits(*xyz_limits, target_dir=head)
+>>>>>>> d06d929 (change branch name and pushed wrong local repo)
 main()
